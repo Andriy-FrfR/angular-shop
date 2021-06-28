f6885fcc3ca62cf56de65d051f61cae9292c8dce
unknown <andriy.lukashewwich@gmail.com>
2021-06-28 18:29:04 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutHeaderComponent } from './components/layout-header/layout-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutHeaderComponent
   ],
   imports: [
     BrowserModule,
